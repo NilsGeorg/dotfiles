c31b284f1fa93cb03bb52ecc512a07703cb7b17e
Nils Georg <n.georg@inits.io>
2024-06-13 16:18:43 +0200

    first dotfiles ever

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,13 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+if not pgrep --full ssh-agent | string collect > /dev/null
+  eval (ssh-agent -c)
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
