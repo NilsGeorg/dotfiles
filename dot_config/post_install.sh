a8bd4d6408ea65ed7409d0c5489a2dfb83523dc7
Nils Georg <mail@ngeorg.de>
2026-07-06 12:21:17 +0200

    script setup stuff

--- dot_config/post_install.sh
+++ dot_config/post_install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# settings
 xdg-settings set default-web-browser librewolf.desktop
-pipx ensurepath
 
 git config --global user.name
 git config --global user.name "Nils Georg"
+
+shelly remove firefox
+
+# pipx
+pipx ensurepath
+pipx install terminaltexteffects
+
+# go
+go install -tags most github.com/xo/usql@latest
